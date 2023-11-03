3a03f51c8215a77489d7bc3499b17bcb1914f741
tim-labahn <tim.labahn@freaknet.de>
2023-11-03 09:00:40 +0000

    "admin" seeder and route to dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,21 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
 
     public function run(): void
     {
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            "password" => Hash::make("admin"),
+            "role" => 'user',
+        ]);
     }
 }
