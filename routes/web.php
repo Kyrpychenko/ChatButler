8c666b1abe5a6c3c2dd7828cddd56a146e8b524e
tim-labahn <tim.labahn@freaknet.de>
2023-11-03 08:51:04 +0000

    Revert "resorting default components"
    This reverts commit 503850770472f377a47b057c1782ca4a0eb4eb58.

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,17 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return redirect("/dashboard");
-})->name('Dashboard');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware("auth")->name('dashboard');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -29,4 +35,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
