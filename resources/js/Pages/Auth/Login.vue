b350e40651e971ce37dd11162b71a1ae4e7ca0b6
tim-labahn <tim.labahn@freaknet.de>
2023-11-13 12:44:53 +0000

    .

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import Checkbox from '@/Components/Checkbox.vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import { Head, useForm } from "@inertiajs/vue3";
+import {
+    Button,
+    CheckboxInput,
+    EmailInput,
+    Message,
+    PasswordInput,
+} from "custom-mbd-components";
+// import { Button } from "custom-mbd-components";
 
 defineProps<{
     canResetPassword?: boolean;
@@ -13,15 +16,15 @@ defineProps<{
 }>();
 
 const form = useForm({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
     remember: false,
 });
 
 const submit = () => {
-    form.post(route('login'), {
+    form.post(route("login"), {
         onFinish: () => {
-            form.reset('password');
+            form.reset("password");
         },
     });
 };
@@ -30,63 +33,44 @@ const submit = () => {
 <template>
     <GuestLayout>
         <Head title="Log in" />
-
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                >
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
+        <Message :info="status" :dismissable="false" />
+        <main class="card card-body p-5">
+            <form @submit.prevent="submit">
+                <h2>Login</h2>
+                <div>
+                    <div>
+                        <EmailInput
+                            v-model="form.email"
+                            required
+                            autofocus
+                            autocomplete="username"
+                            placeholder="Email"
+                            :error="form.errors.email"
+                        />
+                    </div>
+                    <div>
+                        <PasswordInput
+                            v-model="form.password"
+                            required
+                            autofocus
+                            autocomplete="password"
+                            placeholder="Passwort"
+                            :error="form.errors.password"
+                        />
+                    </div>
+                    <div class="mt-3">
+                        <CheckboxInput v-model="form.remember"
+                            >Angemeldet bleiben</CheckboxInput
+                        >
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between mt-3">
+                    <a :href="route('register')" class="p-2 g-col-6 text-dark"
+                        >Registrieren</a
+                    >
+                    <Button :loading="form.processing">Anmelden</Button>
+                </div>
+            </form>
+        </main>
     </GuestLayout>
 </template>
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,16 +1,28 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "../css/app.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-import { createApp, h, DefineComponent } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createApp, h, DefineComponent } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+//FIXME: for some obscure reason we have to import (and use so it doesnt get pruned) vue-resizeable here, or otherwise the calendar cant use it
+//@ts-expect-error
+import VueResizable from "vue-resizable";
+VueResizable;
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>("./Pages/**/*.vue")
+        ),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
@@ -18,6 +30,6 @@ createInertiaApp({
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
